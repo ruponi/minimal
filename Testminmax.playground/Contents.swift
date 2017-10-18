7ba364f26ffdb2b954b64268d9f7af78a2736b4e
Ruslan <ruslan@Ruslans-MacBook-Pro.local>
2017-10-18 14:57:32 +0300

    t1

--- Testminmax.playground/Contents.swift
+++ Testminmax.playground/Contents.swift
@@ -3,34 +3,39 @@
 import UIKit
 
 // test arrays
-var array:[Double] = [4,5,3]
-var array2:[Double] = [4,1,3,6,3,45,22,34,123,6,5,44,342]
+var array:[Double]  = [4,5,3]
+var array2:[Double] = [4,1,3,2,1,21,22]
 var array3:[Double]?
 
 //array is [Double] and contains 3 and more elements
-//returns (0,0) if array not defined
-//return  (0,0) if array have <3 numbers
- private func minmax(data:[Double]?)->(min:NSInteger,max:NSInteger){
-   var  nMinimal=0
-   var  nMaximal=0
+//return  (false) if array not defined
+//return  (false) if array have <3 numbers
+
+
+// return TRUE if quantity of local minima is more than the quantity of local maxima
+// else return FALSE
+ private func minmax(data:[Double]?)->(Bool)
+{
+    var nMinimal=0
+    var nMaximal=0
     
     if let tmpData=data {
         if (tmpData.count < 3){
-            return (0,0)
-    }
-    
-    for index in 1...tmpData.count-2{
-        if tmpData[index]<tmpData[index+1]&&tmpData[index]<tmpData[index-1] {
-            nMinimal += 1
+            return false
         }
-        if tmpData[index]>tmpData[index+1]&&tmpData[index]>tmpData[index-1] {
-            nMaximal += 1
-        }
-    }
     
-        return (nMinimal,nMaximal)
+        for index in 1...tmpData.count-2{
+            if tmpData[index]<tmpData[index+1]&&tmpData[index]<tmpData[index-1] {
+              nMinimal += 1
+            }
+            if tmpData[index]>tmpData[index+1]&&tmpData[index]>tmpData[index-1] {
+                nMaximal += 1
+            }
+        }
+    
+        return (nMinimal>nMaximal)
     } else {
-       return (0,0)
+       return (false)
     }
 }
 
@@ -39,10 +44,9 @@ var array3:[Double]?
 
 
 print ("Result of array 1: \(array) \(minmax(data: array))")
-//Result of array 1: [4.0, 5.0, 3.0]
-//(min: 0, max: 1)
+
 print ("Result of array 2: \(array2) \(minmax(data: array2))")
-//Result of array 2: [4.0, 1.0, 3.0, 6.0, 3.0, 45.0, 22.0, 34.0, 123.0, 6.0, 5.0, 44.0, 342.0]
-//(min: 4, max: 3)
+
 print ("Result of array 3: \(String(describing: array3)) \(minmax(data: array3))")
-//nil (min: 0, max: 0)
+
+
